072b467662900d16bcf7002042963810f9fd4eb5
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-08-16 21:18:02 +0530

    load and print

---
+++ update-prefect-blocks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--block-name")
+parser.add_argument("--command")
+parser.add_argument("--project-dir")
+parser.add_argument("--working-dir")
+parser.add_argument("--profiles-dir")
+args = parser.parse_args()
+
+from prefect_dbt.cli.commands import DbtCoreOperation
+
+if args.block_name:
+    block: DbtCoreOperation = DbtCoreOperation.load(args.block_name, validate=False)
+    print(block.commands[0])
+    print(block.project_dir)
+    print(block.working_dir)
+    print(block.profiles_dir)
